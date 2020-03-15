6cdd29c752a1f604b710871c27f857f065f89f18
jlieth <jlieth@users.noreply.github.com>
2020-03-16 00:06:42 +0100

    HOCRNode class representing a hocr element

--- hocr_formatter/parser.py
+++ hocr_formatter/parser.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import lxml.html
 
 
@@ -6,3 +8,20 @@ class HOCRParser:
         with open(filename, encoding="utf-8") as f:
             data = f.read()
             self.html = lxml.html.document_fromstring(data)
+
+    @property
+    def root(self) -> Optional["HOCRNode"]:
+        element = self.html.find("body")
+        if element is not None:
+            return HOCRNode(element)
+
+
+class HOCRNode:
+    """Wrapper class for a lxml.html.HtmlElement
+
+    This class isn't meant to be used by itself. It is utilised by the
+    HOCRParser class to represent the elements of the HTML tree.
+    """
+
+    def __init__(self, elem: lxml.html.HtmlElement):
+        self.elem = elem
