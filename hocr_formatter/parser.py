3dd9d1f465211ad00ae789acd84a72330cb7ed09
jlieth <jlieth@users.noreply.github.com>
2020-03-16 00:24:14 +0100

    parent property for node, but needs __eq__

--- hocr_formatter/parser.py
+++ hocr_formatter/parser.py
@@ -25,3 +25,21 @@ class HOCRNode:
 
     def __init__(self, elem: lxml.html.HtmlElement):
         self.elem = elem
+
+    @property
+    def parent(self) -> Optional["HOCRNode"]:
+        """Returns the parent node of this node
+
+        The body node is the outer wrapper of the HOCR document and doesn't
+        have a parent.
+
+        :return: HOCRNode, or None
+        """
+        # don't return parent if current node is the body element
+        if self.elem.tag == "body":
+            return None
+
+        # return parent if it exists
+        parent = self.elem.getparent()
+        if parent is not None:
+            return HOCRNode(parent)
--- tests/test_hocr_node.py
+++ tests/test_hocr_node.py
@@ -1,2 +1,21 @@
+import lxml.html
+
+from hocr_formatter.parser import HOCRNode
+
+
 class TestOCRNode:
-    pass
+    @staticmethod
+    def get_body_node_from_string(s: str) -> HOCRNode:
+        document = lxml.html.document_fromstring(s)
+        body = document.find("body")
+        return HOCRNode(body)
+
+    def test_parent(self):
+        body = self.get_body_node_from_string("<body><p>test</p></body>")
+        p = HOCRNode(body.elem.find("p"))
+
+        # body node should return no parent
+        assert body.parent is None
+
+        # p node should return body node as parent
+        assert p.parent == body
