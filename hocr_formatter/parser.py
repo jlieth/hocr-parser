84ce3838571d069a362dd6e399d1960c814bebcc
jlieth <jlieth@users.noreply.github.com>
2020-03-27 13:52:04 +0100

    bbox (for max containing bbox of doc) on parser

--- hocr_formatter/parser.py
+++ hocr_formatter/parser.py
@@ -23,6 +23,23 @@ class HOCRParser:
         if element is not None:
             return element
 
+    @property
+    def bbox(self) -> Optional[BBox]:
+        """Returns the max BBox containing all other BBoxes of tree nodes
+
+        Iterates over the tree, collecting all BBoxes and calls
+        BBox.max_bbox with the list of BBoxes as argument.
+
+        :return: BBox, or None if the list of collected bboxes is empty
+        """
+        boxes = []
+        for node in self.iter():
+            box = node.bbox
+            if box:
+                boxes.append(node.bbox)
+
+        return BBox.max_bbox(boxes)
+
     def iter(self) -> Iterable["HOCRNode"]:
         """Iterates tree in DFS order"""
         queue = [self.root]
