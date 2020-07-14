c0df9e940fa704a8c3a3157e38b890fcae3a9761
jlieth <jlieth@users.noreply.github.com>
2020-07-14 09:00:58 +0200

    some cleanup from renaming parser to document

--- tests/test_hocr_document.py
+++ tests/test_hocr_document.py
@@ -10,9 +10,9 @@ from hocr_parser.hocr_node import HOCRNode
 
 class TestOCRDocument:
     @staticmethod
-    def get_parser_for_file(s: str) -> HOCRDocument:
+    def get_document(filename: str) -> HOCRDocument:
         pwd = os.path.dirname(os.path.abspath(__file__))
-        filepath = os.path.join(pwd, "testdata", s)
+        filepath = os.path.join(pwd, "testdata", filename)
         return HOCRDocument(filepath)
 
     @staticmethod
@@ -22,58 +22,58 @@ class TestOCRDocument:
     def test_init(self):
         # test empty file
         with pytest.raises(EmptyDocumentException):
-            _ = self.get_parser_for_file("test_init_empty_file.hocr")
+            _ = self.get_document("document_test_init_empty_file.hocr")
 
         # test valid file
-        doc = self.get_parser_for_file("test_init_valid_file.hocr")
+        doc = self.get_document("document_test_init_valid_file.hocr")
         assert isinstance(doc.html, HOCRNode)
 
     def test_body(self):
         # test hocr file without body tag
-        p = self.get_parser_for_file("test_body_no_body_tag.hocr")
-        assert p.body is None
+        doc = self.get_document("document_test_body_no_body_tag.hocr")
+        assert doc.body is None
 
         # test hocr file with body tag
-        p = self.get_parser_for_file("test_body_with_body_tag.hocr")
+        doc = self.get_document("document_test_body_with_body_tag.hocr")
         expected = self.get_body_node_from_string("<body><p>Foo</p></body>")
-        assert p.body == expected
+        assert doc.body == expected
 
     def test_ocr_system(self):
         # test hocr file without ocr-system meta tag
-        p = self.get_parser_for_file("document_test_ocr_system_no_meta_tag.hocr")
+        doc = self.get_document("document_test_ocr_system_no_meta_tag.hocr")
 
         with pytest.warns(MissingRequiredMetaField):
-            _ = p.ocr_system
+            _ = doc.ocr_system
 
         # with tag
-        p = self.get_parser_for_file("document_test_ocr_system_with_meta_tag.hocr")
+        doc = self.get_document("document_test_ocr_system_with_meta_tag.hocr")
         expected = "tesseract 4.0.0-beta.1"
-        assert p.ocr_system == expected
+        assert doc.ocr_system == expected
 
     def test_ocr_capabilities(self):
         # no meta tag
-        p = self.get_parser_for_file("document_test_ocr_capabilities_no_tag.hocr")
+        doc = self.get_document("document_test_ocr_capabilities_no_tag.hocr")
 
         with pytest.warns(MissingRequiredMetaField):
-            capabilities = p.ocr_capabilities
+            capabilities = doc.ocr_capabilities
 
         assert capabilities == []
 
-        p = self.get_parser_for_file("document_test_ocr_capabilities_with_tag.hocr")
+        doc = self.get_document("document_test_ocr_capabilities_with_tag.hocr")
         expected = ["ocr_page", "ocr_carea", "ocr_par", "ocr_line", "ocrx_word"]
-        assert p.ocr_capabilities == expected
+        assert doc.ocr_capabilities == expected
 
     def test_bbox(self):
         # no bboxes at all
-        p = self.get_parser_for_file("parser_test_bbox_no_boxes.hocr")
-        assert p.bbox is None
+        doc = self.get_document("document_test_bbox_no_boxes.hocr")
+        assert doc.bbox is None
 
         # one bbox
-        p = self.get_parser_for_file("parser_test_bbox_one_box.hocr")
+        doc = self.get_document("document_test_bbox_one_box.hocr")
         expected = BBox((0, 0, 1000, 1000))
-        assert p.bbox == expected
+        assert doc.bbox == expected
 
         # nested + overlapping bboxes
-        p = self.get_parser_for_file("parser_test_bbox_overlapping_boxes.hocr")
+        doc = self.get_document("document_test_bbox_overlapping_boxes.hocr")
         expected = BBox((25, 25, 1175, 650))
-        assert p.bbox == expected
+        assert doc.bbox == expected
